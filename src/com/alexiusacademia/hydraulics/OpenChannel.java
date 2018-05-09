b8996447e6b253bed40eaf5b3fb655707a9543a9
Alexius Academia <alexius.academia@gmail.com>
2018-05-09 22:24:38 +0800

    Added getter for critical depth

--- src/com/alexiusacademia/hydraulics/OpenChannel.java
+++ src/com/alexiusacademia/hydraulics/OpenChannel.java
@@ -82,6 +82,10 @@ public class OpenChannel {
     return dischargeIntensity;
   }
 
+  public double getCriticalDepth() {
+    return criticalDepth;
+  }
+
   /** ***************************************
    * Setters
    **************************************** */
