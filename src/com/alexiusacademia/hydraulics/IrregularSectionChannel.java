2ac61b31f4f40dca8c0e0523bd7ffdad080124b6
syncster31 <alexius.academia@gmail.com>
2018-05-16 16:28:26 +0800

    Added a class IrrigularSectionChannel. Rename irrigflo.xml to build.xml

---
+++ src/com/alexiusacademia/hydraulics/IrregularSectionChannel.java
@@ -0,0 +1,10 @@
+package com.alexiusacademia.hydraulics;
+
+/**
+ * Irregular section channel
+ * An extension of the class OpenChannel
+ * Used for irregular section channel, usually river channels.
+ */
+public class IrregularSectionChannel extends OpenChannel {
+
+}
